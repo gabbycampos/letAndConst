0922fe7d21e3a19719b7404eee4eed9ac015f631
Gabriela Campos <gabbycampos31@gmail.com>
2020-08-25 17:00:32 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const PI = 3.14;
+PI = 42;
+
+/* Quiz */
+
+/* 
+
+1. What is the difference between var and let?
+Let creates a block-scoped variable that you can reassign but not redeclare. Unlike var.
+
+2. What is the difference between var and const?
+Same as let except you cannot reassign or redeclare with const. 
+
+3. What is the difference between let and const?
+You can reassign with let, but you can't reassign or redeclare with const.
+
+4. What is hoisting?
+When your code is compiled, the variable declarations (ones with var) are ran first.
+Variable declarations with let and const are not hoisted(ran first). 
+Function declarations are hoisted. 
+
+*/
